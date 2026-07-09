1ec2ec12b86eb8ed7f00e93d684bc6c0a9fb609a
ganesh <ganeshgardi7@gmail.com>
2026-07-09 12:09:41 +0530

    added signup and forgot pasword ui.

---
+++ muscle-map_frontend/src/components/Spacer/Spacer.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+interface SpacerProps {
+  size?: number;
+}
+
+export default function Spacer({ size = 16 }: SpacerProps) {
+  return (
+    <View
+      style={{
+        height: size,
+      }}
+    />
+  );
+}
